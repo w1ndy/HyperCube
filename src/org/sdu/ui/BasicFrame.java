5af7f69a1e462d02741ce9f2c6b449980385ad46
Arthas <mystery.wd@gmail.com>
2012-12-20 14:32:55 +0800

    Renamed DraggableBorderlessFrame to BasicFrame.

--- src/org/sdu/ui/BasicFrame.java
+++ src/org/sdu/ui/BasicFrame.java
@@ -1,45 +1,51 @@
 package org.sdu.ui;
 
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
- * DraggableBorderFrame class implements a variant of draggable and
- * borderless JFrame.
+ * Basic class implements the basic UI.
  * 
  * @version 0.1 rev 8000 Dec. 19, 2012.
  * Copyright (c) HyperCube Dev Team.
  */
-public class DraggableBorderlessFrame extends JFrame
+public class BasicFrame extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	
 	private Point mouseCoord = null;
 	
-	public DraggableBorderlessFrame()
+	/**
+	 * Construct a basic frame.
+	 */
+	public BasicFrame()
 	{
 		super();
-		initializeDBF();
-	}
-	
-	public DraggableBorderlessFrame(String title)
-	{
-		super(title);
-		initializeDBF();
+		initializeBasicFrame();
 	}
 	
 	/**
-	 * Initialize the frame.
-	 * Use signature DBF to explicitly distinguish from initializers of subclasses.
+	 * Construct a basic frame with title.
+	 * 
+	 * @param title
 	 */
-	public void initializeDBF()
+	public BasicFrame(String title)
 	{
-		setUndecorated(true);
-		
+		super(title);
+		initializeBasicFrame();
+	}
+	
+	/**
+	 * Make the frame movable.
+	 */
+	private void makeMovable()
+	{
 		addMouseListener(new MouseListener() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {}
@@ -65,11 +71,31 @@ public class DraggableBorderlessFrame extends JFrame
 			@Override
 			public void mouseDragged(MouseEvent arg0) {
 				Point p = arg0.getLocationOnScreen();
-				setLocation(p.x - mouseCoord.x, p.y - mouseCoord.y);
+				if(mouseCoord != null)
+					setLocation(p.x - mouseCoord.x, p.y - mouseCoord.y);
 			}
 
 			@Override
 			public void mouseMoved(MouseEvent arg0) {}
+		});
+	}
+	
+	/**
+	 * Initialize the frame.
+	 */
+	private void initializeBasicFrame()
+	{
+		setUndecorated(true);
+		makeMovable();
+
+		setContentPane(new JPanel() {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void paintComponent(Graphics g)
+			{
+				// TODO paint frame here.
+			}
 		});
 	}
 }
