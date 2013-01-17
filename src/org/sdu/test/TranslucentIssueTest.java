9a06a12eff15b715465c329a9e0a2a2694c1d234
Arthas <mystery.wd@gmail.com>
2013-01-17 17:17:41 +0800

    IME issue demo code. Need clarify.

---
+++ src/org/sdu/test/TranslucentIssueTest.java
@@ -0,0 +1,31 @@
+package org.sdu.test;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import com.sun.awt.AWTUtilities;
+
+@SuppressWarnings("serial")
+public class TranslucentIssueTest extends JFrame
+{
+	public static void main(String[] args)
+	{
+		(new TranslucentIssueTest()).setVisible(true);
+	}
+	
+	public TranslucentIssueTest()
+	{
+		super();
+		setUndecorated(true);
+		setLayout(new FlowLayout());
+		setSize(300, 300);
+		AWTUtilities.setWindowOpaque(this, false);
+		
+		JTextField box = new JTextField();
+		//box.setBounds(30, 150, 100, 25);
+		add(box);
+	}
+}
